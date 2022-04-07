54e8b5f527f592fe0c13a72043dfb3d178b2a5a2
SINHS04 <102716326+SINHS04@users.noreply.github.com>
2022-04-08 00:40:34 +0900

    어떻게 실행되는지 감만
    어떻게 실행되는지 감만

--- gui.py
+++ gui.py
@@ -23,21 +23,22 @@ lab_topic.pack()
 
 # INPUT 프레임
 input_frame = LabelFrame(root)
-input_frame.pack()
+input_frame.pack(side = "top")
 
 # PROCESS 프레임
 process_frame = LabelFrame(root)
-process_frame.pack()
+process_frame.pack(side="top", fill="both", expand=True)
 
 # PROCESSOR 프레임
 processor_frame = LabelFrame(root)
-processor_frame.pack()
+processor_frame.pack(side="top", fill="both", expand=True)
 
 # SCHEDULING TYPE 옵션
 lab_scheduling_type = Label(input_frame, text="SCHEDULING TYPE", width=15)
 lab_scheduling_type.grid(column = 1, row = 0, padx = 10, pady = 10)
 
 opt_scheduling_type = ["FCFS", "RR", "SPN", "SRTN", "HRRN"]
+
 cmb_scheduling_type = ttk.Combobox(input_frame, state="readonly", values=opt_scheduling_type, width=8)
 cmb_scheduling_type.current(0)
 cmb_scheduling_type.grid(column = 2, row = 0, padx = 10, pady = 10)
@@ -49,17 +50,57 @@ lb1_width.grid(column = 3, row = 0, padx = 10, pady = 10)
 txt = Text(input_frame, width=10, height=1)
 txt.grid(column = 4, row = 0, padx = 10, pady = 10)
 
+
+p_core = 0
+e_core = 0
+core = []
+
+# add p core
+def add_p_core():
+    global p_core
+    global e_core
+    global core
+    if p_core + e_core < 4:
+        CORE = Label(processor_frame, text="P CORE", width=10)
+        CORE.pack()
+        p_core += 1
+        core.append('p')
+
+def add_e_core():
+    global p_core
+    global e_core
+    global core
+    if p_core + e_core < 4:
+        CORE = Label(processor_frame, text="E CORE", width=10)
+        CORE.pack()
+        e_core += 1
+        core.append('e')
+
+def delete_core():
+    global p_core
+    global e_core
+    global core
+    if p_core + e_core > 0:
+        processor_frame.pack_slaves().pop().destroy()
+        if core.pop == 'p':
+            p_core -= 1
+        else:
+            e_core -= 1
+
 # P CORE ADD BUTTON
 btn_processorP_add = Button(input_frame, padx=5, pady=5, width=10, text="ADD P CORE")
 btn_processorP_add.grid(column = 5, row = 0, padx = 10, pady = 10)
+btn_processorP_add.config(command = add_p_core)
 
 # E CORE ADD BUTTON
 btn_processorE_add = Button(input_frame, padx=5, pady=5, width=10, text="ADD E CORE")
 btn_processorE_add.grid(column = 6, row = 0, padx = 10, pady = 10)
+btn_processorE_add.config(command = add_e_core)
 
 # CORE DELETE BUTTON
 btn_processor_delete = Button(input_frame, padx=5, pady=5, width=11, text="CORE DELETE")
 btn_processor_delete.grid(column = 7, row = 0, padx = 10, pady = 10)
+btn_processor_delete.config(command = delete_core)
 
 # # P CORE  옵션
 # lb1_width = Label(input_frame, text="P", width=15)
@@ -79,13 +120,31 @@ btn_processor_delete.grid(column = 7, row = 0, padx = 10, pady = 10)
 # cmb_width.current(0)
 # cmb_width.grid(column = 6, row = 0, padx = 10, pady = 10)
 
+process = 0
+
+# add p core
+def add_process():
+    global process
+    if process < 15:
+        process += 1
+        PROCESS = Label(process_frame, text=("P", process), width=10)
+        PROCESS.pack()
+
+def delete_process():
+    global process
+    if process > 0:
+        process_frame.pack_slaves().pop().destroy()
+        process -= 1
+
 # PROCESS ADD BUTTON
 btn_process_add = Button(input_frame, padx=5, pady=5, width=5, text="ADD")
 btn_process_add.grid(column = 8, row = 0, padx = 10, pady = 10)
+btn_process_add.config(command = add_process)
 
 # PROCESS DELETE BUTTON
 btn_process_delete = Button(input_frame, padx=5, pady=5, width=5, text="DELETE")
 btn_process_delete.grid(column = 9, row = 0, padx = 10, pady = 10)
+btn_process_delete.config(command = delete_process)
 
 # RUN BUTTON
 btn_run = Button(input_frame, padx=5, pady=5, width=5, text="RUN", bg="yellow")
