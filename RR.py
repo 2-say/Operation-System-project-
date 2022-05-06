349e4bf9905a3dcbd490c58290397b831ec17f3d
SINHS04 <102716326+SINHS04@users.noreply.github.com>
2022-05-07 00:22:57 +0900

    경우 수정
    time quantum 끝나서 레디큐에 들어가는 프로세스랑 at때문에 레디큐에 들어가는 프로세스가 겹칠 때 at 우선 실행하는거로 변경
    + readyQ 수정

--- RR.py
+++ RR.py
@@ -1,4 +1,3 @@
-
 import col_gantt
 import time_calculator
 import copy
@@ -12,7 +11,7 @@ def rr(at, bt, pn, time_quantum, gantt_default):
     end_time = [None] * len(at)  # end_time
     gantt = [["" for j in range(min(at) + 1)] for j in range(pn)]  # make empty gantt 2 dimensional list
     power_used = 0
-    wtime = [0] * len(at)#NEW
+    wtime = [0] * len(at)  # NEW
     tq_wait_queue = []  # time_quantum ready_q
     t_q_counter = [time_quantum] * pn  # calculate last time_quantum
     ready_Q = [[]]
@@ -24,8 +23,124 @@ def rr(at, bt, pn, time_quantum, gantt_default):
         if timer in at:  # Arrival_Time -> Ready_Queue
             ready_queue.append(at.index(timer))
         while len(tq_wait_queue) != 0:
+            ready_queue.append(tq_wait_queue.pop())
+
+        for processor_n in range(pn):  # Processor(Core) -> 0부터 시작
+            gantt[processor_n].append('')
+            if processor_n in line:  # 만약 라인 리스트에 n번째 프로세서가 잡히면
+                process_num = line.index(processor_n)  # 프로세스 추출
+                if gantt[processor_n][0] == 'P':  # Processor == 'P'
+                    bt[process_num] -= 2  # 실행 시간 -2 (P)
+                    power_used += 3  # 3W (P)
+                    used_core += 1  # 전력 소비
+                    t_q_counter[processor_n] -= 1
+                    if t_q_counter[processor_n] == 0:  # 만약 time_qauntum 만큼 사용했다면
+                        line[process_num] = 'None'  # burst 남아있다는 기준 하에 tq_wait_queue에 넣고 다음 차례 시작
+                        if bt[process_num] > 0:
+                            tq_wait_queue.append(process_num)
+                        else:
+                            end_time[process_num] = timer + 1
+                    elif bt[process_num] <= 0:  # 만약 실행시간이 0이하 (P일 때 -1도 될수 있으니)
+                        line[process_num] = 'None'  # 라인큐에서 제거
+                        end_time[process_num] = timer + 1
+                    t_q_counter[processor_n] = time_quantum  # time_qauntum 초기하
+                else:  # Processor == 'E'
+                    bt[process_num] -= 1  # 실행 시간 -1 (E)
+                    power_used += 1  # 1W (E)
+                    used_core += 1  # 전력 소비
+                    t_q_counter[processor_n] -= 1
+                    if t_q_counter[processor_n] == 0:
+                        line[process_num] = 'None'
+                        if bt[process_num] > 0:
+                            tq_wait_queue.append(process_num)
+                        else:
+                            end_time[process_num] = timer + 1
+                    elif bt[process_num] <= 0:  # 만약 실행시간이 0이하 (P일 때 -1도 될수 있으니)
+                        line[process_num] = 'None'  # 라인큐에서 나가리
+                        end_time[process_num] = timer + 1
+                    t_q_counter[processor_n] = time_quantum
+                gantt[processor_n][timer + 1] = col_gantt.colors(process_num)  # 간트에 집어 넣음
+
+            elif len(ready_queue) != 0:  # 레디큐가 안비워져 있으면 && line에 잡히는 것이 없다면
+                process_num = ready_queue.pop(0)  # 첫번째 값을 꺼내고 삭제한다.
+
+                if gantt[processor_n][0] == 'P':
+                    bt[process_num] -= 2  # 실행 시간 -2 (P)
+                    power_used += 3  # 3W (P)
+                    used_core += 1  # 전력 소비
+                else:
+                    bt[process_num] -= 1  # 실행 시간 -1 (E)
+                    power_used += 1  # 1W (E)
+                    used_core += 1  # 전력 소비
+
+                gantt[processor_n][timer + 1] = col_gantt.colors(process_num)  # 간트에 집어 넣음
+
+                if bt[process_num] > 0:
+                    if t_q_counter[processor_n] != 1:
+                        line[process_num] = processor_n  # 해당 번째 processor를 다음에도 사용하겠습니다.
+                        t_q_counter[processor_n] -= 1
+                    else:
+                        tq_wait_queue.append(process_num)
+                else:
+                    end_time[process_num] = timer + 1
+
+            else:  # 빈 것 -> 흰 것
+                gantt[processor_n][timer + 1] = 'White'
+
+            power_used += (0.1 * (pn - used_core))  # 대기 전력
+
+        ready_Q.append(copy.deepcopy(ready_queue))
+
+        if max(bt) <= 0:
+            break
+
+        for i in ready_queue:
+            wtime[i] += 1
+
+        timer += 1
+
+    ttime = time_calculator.turnaround_time(at, end_time)
+    ntime = time_calculator.normalized_tt(ttime, bt_copy)
+
+    # remvoe '' element
+
+    for i in range(len(gantt)):  # 세로 크기
+        for j in range(len(gantt[i])):  # 가로 크기
+            gantt[i] = ' '.join(gantt[i]).split()
+
+    return gantt, power_used, ttime, wtime, ntime, ready_Q
+
+
+"""
+import col_gantt
+import time_calculator
+import copy
+
+
+def rr(at, bt, pn, time_quantum, gantt_default):
+    bt_copy = copy.deepcopy(bt)  # waiting , ntt를 계산하기 위해 원복 복사
+    ready_queue = []  # 레디 큐
+    line = [None] * len(at)  # 라인 리스트 (연속적인 입력을 위한 기억 리스트)
+    timer = 0  # 타이머
+    end_time = [None] * len(at)  # end_time
+    gantt = [["" for j in range(sum(bt) + 10)] for j in range(pn)]  # make empty gantt 2 dimensional list
+    power_used = 0
+    wtime = [0] * len(at)#NEW
+    tq_wait_queue = []  # time_quantum ready_q
+    t_q_counter = [time_quantum] * pn  # calculate last time_quantum
+
+    ready_Q = [[]]
+
+    for i in range(pn):
+        gantt[i][0] = gantt_default[i]  # 간트 차트 초기값 'P','E' 입력
+    while True:  # 무한 반복
+        used_core = 0  # 전력이 소비된 프로세서
+        if timer in at:  # Arrival_Time -> Ready_Queue
+            ready_queue.append(at.index(timer))
+        while len(tq_wait_queue) != 0:
             ready_queue.append(tq_wait_queue.pop(0))
         for processor_n in range(pn):  # Processor(Core) -> 0부터 시작
+
             gantt[processor_n].append('')
             if processor_n in line:  # 만약 라인 리스트에 n번째 프로세서가 잡히면
                 process_num = line.index(processor_n)   # 프로세스 추출
@@ -63,7 +178,7 @@ def rr(at, bt, pn, time_quantum, gantt_default):
 
             elif len(ready_queue) != 0:  # 레디큐가 안비워져 있으면 && line에 잡히는 것이 없다면
                 process_num = ready_queue.pop(0)  # 첫번째 값을 꺼내고 삭제한다.
-                
+
                 if gantt[processor_n][0] == 'P':
                     bt[process_num] -= 2  # 실행 시간 -2 (P)
                     power_used += 3  # 3W (P)
@@ -72,15 +187,13 @@ def rr(at, bt, pn, time_quantum, gantt_default):
                     bt[process_num] -= 1  # 실행 시간 -1 (E)
                     power_used += 1  # 1W (E)
                     used_core += 1  # 전력 소비
-                
+
                 gantt[processor_n][timer + 1] = col_gantt.colors(process_num)  # 간트에 집어 넣음
-                
+
                 if bt[process_num] > 0:
-                    if t_q_counter[processor_n] != 1 :
+                    if  time_quantum != 1  :
                         line[process_num] = processor_n  # 해당 번째 processor를 다음에도 사용하겠습니다.
                         t_q_counter[processor_n] -= 1
-                    else :
-                        tq_wait_queue.append(process_num)
                 else:
                     end_time[process_num] = timer + 1
 
@@ -93,10 +206,13 @@ def rr(at, bt, pn, time_quantum, gantt_default):
             break
 
 
+
         for i in ready_queue :
             wtime[i] += 1
 
-        ready_Q.append(copy.deepcopy(ready_queue))  
+
+
+        ready_Q.append(copy.deepcopy(ready_queue))
         timer += 1
 
     ttime = time_calculator.turnaround_time(at, end_time)
@@ -113,6 +229,8 @@ def rr(at, bt, pn, time_quantum, gantt_default):
 
 
 
-            
+
     return gantt, power_used, ttime, wtime, ntime ,ready_Q
 
+
+"""
