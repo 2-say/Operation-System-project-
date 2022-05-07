6e6061253b71ee60f574470ab07a2dbf061cc676
SINHS04 <102716326+SINHS04@users.noreply.github.com>
2022-05-07 19:35:43 +0900

    거의 최종
    거의 최종

--- time_calculator.py
+++ time_calculator.py
@@ -1,19 +1,14 @@
-def turnaround_time(at, et) : 
-    tt = [None] * len(at)  #out of range issue problem 
-    
-    for i in range(0,len(at)):
-        tt[i] = et[i] - at[i]  
+def turnaround_time(at, et):
+    tt = [None] * len(at)
+    for i in range(0, len(at)):
+        tt[i] = et[i] - at[i]
     return tt
 
-def normalized_tt(tt, bt) :
-    ntt= [None] * len(bt)
-    #ntt = [] 
-    for i in range(0,len(bt)):
-        if(bt[i]>0):
-            ntt[i] = tt[i]/bt[i]
-        else: ntt[i] = 0  
+def normalized_tt(tt, bt):
+    ntt = [None] * len(bt)
+    for i in range(0, len(bt)):
+        if bt[i] > 0:
+            ntt[i] = tt[i] / bt[i]
+        else:
+            ntt[i] = 0
     return ntt
-
-
-
-    
