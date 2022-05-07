ddf16ecb73c992e426201fd30f4badf499e58bc0
SINHS04 <102716326+SINHS04@users.noreply.github.com>
2022-05-07 19:34:27 +0900

    거의 최종
    거의 최종

--- col_gantt.py
+++ col_gantt.py
@@ -1,5 +1,6 @@
-def colors(col_num) :
-    color_dic = {-1:"white",0:"p1_color",1:"p2_color",2:"p3_color",3:"p4_color",
-    4:"p5_color",5:"p6_color",6:"p7_color",7:"p8_color",8:"p9_color",9:"p10_color",10:"p11_color"}
-     
+def colors(col_num):
+    color_dic = {-1: "white", 0: "p1_color", 1: "p2_color", 2: "p3_color", 3: "p4_color",
+                 4: "p5_color", 5: "p6_color", 6: "p7_color", 7: "p8_color", 8: "p9_color", 9: "p10_color",
+                 10: "p11_color", 11: "p12_color", 12: "p13_color", 13: "p14_color", 14: "p15_color"}
+
     return color_dic[col_num]
